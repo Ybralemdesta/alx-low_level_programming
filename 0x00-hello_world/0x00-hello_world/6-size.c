e16dd172a94bb7a2089afa32da655d95f1425047
Ybralemdesta <ybralemdesta@gmail.com>
2022-12-09 17:48:46 -0800

    task 6

---
+++ 0x00-hello_world/0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %1u byte(s)\n", sizeof(char));
+	printf("size of an int: %1u byte(s)\n", sizeof(int));
+	printf("size of a long int: %1u byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %1u byte(s)\n", sizeof(long long int));
+	printf("size of a float: %1u byte(s)\n", sizeof(float));
+	return (0);
+}
