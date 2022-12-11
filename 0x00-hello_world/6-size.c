3e03f5a572e979669160a98c063fe238f2b8baaf
Ybralemdesta <ybralemdesta@gmail.com>
2022-12-10 16:33:25 -0800

    task 0

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main-Entry
+ * Return:Always 0
+ */
+int main(void)
+{
+	printf ("Size of a char: %i byte(s)\n", Size of a (char));
+       printf ("Size of an int: %ibyte(s)\n", Size of an (int));
+      printf ("Size of a long int:%ibyte(s)\n",size of (long int));
+      printf ("Size of a long long int:%ibyte(s)\n",size of (long long int));
+       printf ("Size of a float:%ibyte(s)\n",size of (float));
+       return(0);
+
